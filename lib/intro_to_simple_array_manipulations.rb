0f558e1efcf02b8f4502a3ea6eb712436cd4fe6f
David Jun <djun301@gmail.com>
2021-01-01 22:32:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(string)
+  thing = string.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  im_so_over_this_city = my_favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  brands_removed = ice_cream_brands.shift(2)
+end
